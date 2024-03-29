d5c1f6c499a901b60d97af89870c46608f218f56
Mert <88289398+Deta0ne@users.noreply.github.com>
2024-03-29 17:21:46 +0000

    env file added

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
-export const GEO_API_KEY = 'd6bcaed461msh5cce96a5fd5a4aap114f42jsn2f112ee15ee8';
-export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = 'f20ff7b5902b0478106d719ad72102be';
-export const UV_API_KEY = 'openuv-3i96anyrlubgqmay-io';
-export const UV_API_URL = 'https://api.openuv.io/api/v1';
+export const GEO_API_URL = import.meta.env.VITE_GEO_API_URL;
+export const GEO_API_KEY = import.meta.env.VITE_GEO_API_KEY;
+export const WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_URL;
+export const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+export const UV_API_KEY = import.meta.env.VITE_UV_API_KEY;
+export const UV_API_URL = import.meta.env.VITE_UV_API_URL;
